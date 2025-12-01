f6ad546eb3ab2d4190930445fe0d841e0c0be223
KhanhHoa212 <khanhhoakt2k4@gmail.com>
2025-12-01 11:14:38 +0700

    todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css"; 
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleAddToList = () => {
+    if (input.trim() === "") return;
+    setTodos(pre => [...pre, { id: uuidv4(), text: capitalizeWord(input) }]);
+    setInput("");
+  };
+
+  const handleInputValue = (e) => setInput(e.target.value);
+
+  const handleEnterPress = (e) => {
+    if (e.key === "Enter") handleAddToList();
+  };
+
+  const handleDeleteTask = (id) => {
+    setTodos(pre => pre.filter(todo => todo.id !== id));
+  };
+
+  const capitalizeWord = (word) => {
+    if (!word) return "";
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo App Mini</h1>
+      <div className="input-group">
+        <input
+          type="text"
+          onKeyDown={handleEnterPress}
+          onChange={handleInputValue}
+          value={input}
+        />
+        <button onClick={handleAddToList}>Add to list</button>
+      </div>
+      <h3>Danh sách các nhiệm vụ</h3>
+      <ul className="todo-list">
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => handleDeleteTask(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
