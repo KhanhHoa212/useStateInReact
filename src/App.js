3ee564d3107e9df5e7f6390c4b124c100470dc23
KhanhHoa212 <khanhhoakt2k4@gmail.com>
2025-12-01 17:20:56 +0700

    Filter following status of task

--- src/App.js
+++ src/App.js
@@ -1,31 +1,71 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import "./App.css"; 
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+
+  const storageTask = JSON.parse(localStorage.getItem('tasks'));
+
+  const [todos, setTodos] = useState(storageTask ?? []);
   const [input, setInput] = useState("");
+  const [filter, setFilter] = useState("all");
 
   const handleAddToList = () => {
     if (input.trim() === "") return;
-    setTodos(pre => [...pre, { id: uuidv4(), text: capitalizeWord(input) }]);
+
+    setTodos(pre => {
+      const newJob = [...pre, { id: uuidv4(), text: capitalizeWord(input), status: false }]
+
+      const jsonJobs = JSON.stringify(newJob);
+      localStorage.setItem('tasks', jsonJobs);
+      return newJob;
+    }
+    );
+
     setInput("");
   };
+  const handleDeleteTask = (id) => {
+    setTodos(pre => {
+      const newTodos = pre.filter(todo => todo.id !== id);
+      localStorage.setItem('tasks', JSON.stringify(newTodos));
+      return newTodos;
+    }
+    );
+  };
 
-  const handleInputValue = (e) => setInput(e.target.value);
+  const handleMarkStatusTask = (id) => {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, status: !todo.status }
+      }
+      return todo;
+    }
+    )
+    setTodos(newTodos);
+    localStorage.setItem('tasks', JSON.stringify(newTodos));
+
+  }
+
+
 
   const handleEnterPress = (e) => {
     if (e.key === "Enter") handleAddToList();
   };
 
-  const handleDeleteTask = (id) => {
-    setTodos(pre => pre.filter(todo => todo.id !== id));
-  };
+
 
   const capitalizeWord = (word) => {
     if (!word) return "";
     return word.charAt(0).toUpperCase() + word.slice(1);
   };
+
+  const filterTodo = useMemo(() => {
+    if (filter === "done") return todos.filter(t => t.status)
+    if (filter === "not") return todos.filter(t => !t.status) 
+    return todos
+  }, [todos, filter])
+
+
 
   return (
     <div className="App">
@@ -34,22 +74,29 @@ function App() {
         <input
           type="text"
           onKeyDown={handleEnterPress}
-          onChange={handleInputValue}
+          onChange={(e) => setInput(e.target.value)}
           value={input}
         />
         <button onClick={handleAddToList}>Add to list</button>
       </div>
-      <h3>Danh sách các nhiệm vụ</h3>
-      <select>
-        <option>All task</option>
-        <option>Done</option>
-        <option>Not yet</option>
-      </select>
+      <div className="header-row ">
+        <h3>Danh sách các nhiệm vụ</h3>
+        <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+          <option value="all">All task</option>
+          <option value="done">Done</option>
+          <option value="not">Not yet</option>
+        </select>
+      </div>
       <ul className="todo-list">
-        {todos.map(todo => (
+        {filterTodo.map(todo => (
           <li key={todo.id}>
-            {todo.text}
-            <button onClick={() => handleDeleteTask(todo.id)}>Delete</button>
+            <span style={{ textDecoration: todo.status ? "line-through" : "none" }}>
+              {todo.text}
+            </span>
+            <div>
+              <button className="delete" onClick={() => handleDeleteTask(todo.id)}>Delete</button>
+              <input type="checkbox" checked={todo.status} onChange={() => handleMarkStatusTask(todo.id)} />
+            </div>
           </li>
         ))}
       </ul>
